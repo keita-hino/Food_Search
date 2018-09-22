d5f430a48bdc34ec41c7b9ab5ed16b490ff4443e
日野恵太 <hinokeita@hinokeitanoMacBook-puro.local>
2018-09-22 21:51:29 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
-  Rails.application.routes.draw do
-    post '/callback' => 'linebot#callback'
-  end
+  post '/callback' => 'linebot#callback'  
 end
