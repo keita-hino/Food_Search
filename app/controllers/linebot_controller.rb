21b7c064e87a0a3f6e10b58c92fe8eaa0029f4d0
日野恵太 <hinokeita@hinokeitanoMacBook-puro.local>
2018-09-23 11:45:22 +0900

    test

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -34,7 +34,7 @@ class LinebotController < ApplicationController
         when Line::Bot::Event::MessageType::Location
           message = {
             type: 'text',
-            text: get_message_content(ENV["LINE_CHANNEL_SECRET"])
+            text: event.message
           }
           client.reply_message(event['replyToken'], message)
         end
