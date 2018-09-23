09fa2e0036b360a46e6dfb0c382a1288dd5791bd
日野恵太 <hinokeita@hinokeitanoMacBook-puro.local>
2018-09-23 15:51:22 +0900

    test

---
+++ app/lib/processing.rb
@@ -0,0 +1,31 @@
+
+
+class Processer
+  attr_accessor :json
+  def initialize(json)
+    @json = json
+  end
+
+  def extraction
+    category = []
+    buf = ""
+
+    #LINEで送る文章作成
+    for i in 0..count - 1
+      buf << "店名：#{rest[i]["name"]}\n"
+      buf << "URL：#{rest[i]["url_mobile"]}\n"
+      buf << "ジャンル：#{rest[i]["category"]}\n"
+      buf << "アクセス：#{rest[i]["address"]}\n\n"
+    end
+
+    puts buf
+  end
+
+  def rest
+    json["rest"]
+  end
+
+  def count
+    json["hit_per_page"].to_i
+  end
+end
