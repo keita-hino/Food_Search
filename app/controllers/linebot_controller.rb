5ca718c43ce5de61ac7d60f4d262496044adfe4f
日野恵太 <hinokeita@hinokeitanoMacBook-puro.local>
2018-09-23 10:51:18 +0900

    test

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -34,7 +34,7 @@ class LinebotController < ApplicationController
         when Line::Bot::Event::MessageType::Location
           message = {
             type: 'text',
-            text: event.message['text']
+            text: event.message['location']
           }
           client.reply_message(event['replyToken'], message)
         end
