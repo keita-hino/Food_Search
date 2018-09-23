7a4e6f55dd66918a23c2b8b77bf225ba1b6408eb
日野恵太 <hinokeita@hinokeitanoMacBook-puro.local>
2018-09-23 15:30:52 +0900

    test

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -45,7 +45,7 @@ class LinebotController < ApplicationController
 
           message = {
             type: 'text',
-            text: reply_text
+            text: "test"
           }
           client.reply_message(event['replyToken'], message)
         end
