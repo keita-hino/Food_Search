ea56b0ef2a9a0a1b0228e16aed63d02c68c33ca6
日野恵太 <hinokeita@hinokeitanoMacBook-puro.local>
2018-09-23 17:08:37 +0900

    test

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -41,15 +41,11 @@ class LinebotController < ApplicationController
 
           lat = event.message['latitude']
           lon = event.message['longitude']
-
-          search = Searcher.new(lat.to_i,lon.to_i)
-          json = search.get_info
-          pro = Processer.new(json)
-          a = pro.extraction
+          reply_text = food_search(lat,lon)
 
           message = {
             type: 'text',
-            text: a
+            text: locate
           }
           client.reply_message(event['replyToken'], message)
         end
@@ -59,14 +55,14 @@ class LinebotController < ApplicationController
     head :ok
   end
 
-  # def food_search(lat,lon)
-  #   # latitude = 38.444207
-  #   # longitude = 141.288718
-  #
-  #   search = Searcher.new(lat.to_i,lon.to_i)
-  #   json = search.get_info
-  #   pro = Processer.new(json)
-  #   return pro.extraction
-  # end
+  def food_search(lat,lon)
+    # latitude = 38.444207
+    # longitude = 141.288718
+
+    search = Searcher.new(lat.to_i,lon.to_i)
+    json = search.get_info
+    pro = Processer.new(json)
+    return pro.extraction
+  end
 
 end
