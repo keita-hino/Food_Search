23f316709215bbd5e4ef04593065ad5fc3e3658f
日野恵太 <hinokeita@hinokeitanoMacBook-puro.local>
2018-09-23 16:48:02 +0900

    test

--- app/lib/processing.rb
+++ app/lib/processing.rb
@@ -10,18 +10,13 @@ class Processer
     category = []
     buf = ""
 
-    buf << "店名：#{rest[0]["name"]}\n"
-    # buf << "URL：#{rest[0]["url_mobile"]}\n"
-    # buf << "ジャンル：#{rest[0]["category"]}\n"
-    # buf << "アクセス：#{rest[0]["address"]}\n\n"
-
-    # #LINEで送る文章作成
-    # for i in 0..count - 5
-    #   buf << "店名：#{rest[i]["name"]}\n"
-    #   buf << "URL：#{rest[i]["url_mobile"]}\n"
-    #   buf << "ジャンル：#{rest[i]["category"]}\n"
-    #   buf << "アクセス：#{rest[i]["address"]}\n\n"
-    # end
+    #LINEで送る文章作成
+    for i in 0..count - 1
+      buf << "店名：#{rest[i]["name"]}\n"
+      buf << "URL：#{rest[i]["url_mobile"]}\n"
+      buf << "ジャンル：#{rest[i]["category"]}\n"
+      buf << "アクセス：#{rest[i]["address"]}\n\n"
+    end
 
     return buf
   end
