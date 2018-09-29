58624f05a48fb691806b0508228f9967b48a1a20
日野恵太 <hinokeita@hinokeitanoMacBook-puro.local>
2018-09-29 15:11:58 +0900

    test

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -50,7 +50,7 @@ class LinebotController < ApplicationController
             },
             hero: {
               type: "image",
-              url: https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_2_restaurant.png,
+              url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_2_restaurant.png",
               size: "full",
               aspectRatio: "4:3"
             },
