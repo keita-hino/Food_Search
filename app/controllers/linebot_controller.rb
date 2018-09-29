8c799298c12748c41b99f1f9586d03f8526cc9d0
日野恵太 <hinokeita@hinokeitanoMacBook-puro.local>
2018-09-29 16:09:24 +0900

    test

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -166,140 +166,139 @@ class LinebotController < ApplicationController
                   }
                 ]
               }
-            }
-            type: "flex",
-            altText: "this is a flex message",
-            contents: {
-              type: "bubble",
-              hero: {
-                type: "image",
-                url: HORIZONTAL_THUMBNAIL_URL,
-                size: "full",
-                aspectRatio: "20:13"
-              },
-              body: {
-                type: "box",
-                layout: "vertical",
-                contents: [
-                  {
-                    type: "text",
-                    text: "スターバックスコーヒー",
-                    weight: "bold",
-                    size: "xl"
-                  },
-                  {
-                    type: "box",
-                    layout: "baseline",
-                    margin: "md",
-                    # 星の評価の部分
-                    contents: [
-                      {
-                        type: "icon",
-                        size: "sm",
-                        url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                      },
-                      {
-                        type: "icon",
-                        size: "sm",
-                        url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                      },
-                      {
-                        type: "icon",
-                        size: "sm",
-                        url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                      },
-                      {
-                        type: "icon",
-                        size: "sm",
-                        url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                      },
-                      {
-                        type: "icon",
-                        size: "sm",
-                        url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-                      },
-                      {
+            },            type: "flex",
+                        altText: "this is a flex message",
+                        contents: {
+                          type: "bubble",
+                          hero: {
+                            type: "image",
+                            url: HORIZONTAL_THUMBNAIL_URL,
+                            size: "full",
+                            aspectRatio: "20:13"
+                          },
+                          body: {
+                            type: "box",
+                            layout: "vertical",
+                            contents: [
+                              {
+                                type: "text",
+                                text: "スターバックスコーヒー",
+                                weight: "bold",
+                                size: "xl"
+                              },
+                              {
+                                type: "box",
+                                layout: "baseline",
+                                margin: "md",
+                                # 星の評価の部分
+                                contents: [
+                                  {
+                                    type: "icon",
+                                    size: "sm",
+                                    url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                                  },
+                                  {
+                                    type: "icon",
+                                    size: "sm",
+                                    url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                                  },
+                                  {
+                                    type: "icon",
+                                    size: "sm",
+                                    url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                                  },
+                                  {
+                                    type: "icon",
+                                    size: "sm",
+                                    url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                                  },
+                                  {
+                                    type: "icon",
+                                    size: "sm",
+                                    url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
+                                  },
+                                  {
 
-                        type: "text",
-                        text: "4.0",
-                        size: "sm",
-                        color: "#999999",
-                        margin: "md",
-                        flex: 0
-                      }
-                    ]
-                  },
-                {
-                  type: "box",
-                  layout: "vertical",
-                  margin: "lg",
-                  spacing: "sm",
-                  contents: [
-                    {
-                      type: "box",
-                      layout: "baseline",
-                      spacing: "sm",
-                      contents: [
-                        {
-                          type: "text",
-                          text: "Place",
-                          color: "#aaaaaa",
-                          size: "sm",
-                          flex: 1
-                        },
-                        {
-                          type: "text",
-                          text: "宮城県仙台市青葉区中央2-2-38 フォーシーズンビル8F",
-                          wrap: true,
-                          color: "#666666",
-                          size: "sm",
-                          flex: 5
+                                    type: "text",
+                                    text: "4.0",
+                                    size: "sm",
+                                    color: "#999999",
+                                    margin: "md",
+                                    flex: 0
+                                  }
+                                ]
+                              },
+                            {
+                              type: "box",
+                              layout: "vertical",
+                              margin: "lg",
+                              spacing: "sm",
+                              contents: [
+                                {
+                                  type: "box",
+                                  layout: "baseline",
+                                  spacing: "sm",
+                                  contents: [
+                                    {
+                                      type: "text",
+                                      text: "Place",
+                                      color: "#aaaaaa",
+                                      size: "sm",
+                                      flex: 1
+                                    },
+                                    {
+                                      type: "text",
+                                      text: "宮城県仙台市青葉区中央2-2-38 フォーシーズンビル8F",
+                                      wrap: true,
+                                      color: "#666666",
+                                      size: "sm",
+                                      flex: 5
+                                    }
+                                  ]
+                                },
+                                {
+                                  type: "box",
+                                  layout: "baseline",
+                                  spacing: "sm",
+                                  contents: [
+                                    {
+                                      type: "text",
+                                      text: "Time",
+                                      color: "#aaaaaa",
+                                      size: "sm",
+                                      flex: 1
+                                    },
+                                    {
+                                      type: "text",
+                                      text: "10:00 - 23:00",
+                                      wrap: true,
+                                      color: "#666666",
+                                      size: "sm",
+                                      flex: 5
+                                    }
+                                  ]
+                                }
+                              ]
+                            }
+                            ]
+                          },
+                          footer: {
+                            type: "box",
+                            layout: "vertical",
+                            contents: [
+                              {
+                                type: "button",
+                                style: "link",
+                                height: "sm",
+                                  action: {
+                                  type: "uri",
+                                  label: "WEBSITE",
+                                  uri: "http://mobile.gnavi.co.jp/shop/t006913/?ak=FttuwHWr4RPF4xpOulk3Z1ouZrp1pzxSR2g80sgnVZc%3D"
+                                }
+                              }
+                            ]
+                          }
                         }
-                      ]
-                    },
-                    {
-                      type: "box",
-                      layout: "baseline",
-                      spacing: "sm",
-                      contents: [
-                        {
-                          type: "text",
-                          text: "Time",
-                          color: "#aaaaaa",
-                          size: "sm",
-                          flex: 1
-                        },
-                        {
-                          type: "text",
-                          text: "10:00 - 23:00",
-                          wrap: true,
-                          color: "#666666",
-                          size: "sm",
-                          flex: 5
-                        }
-                      ]
-                    }
-                  ]
-                }
-                ]
-              },
-              footer: {
-                type: "box",
-                layout: "vertical",
-                contents: [
-                  {
-                    type: "button",
-                    style: "link",
-                    height: "sm",
-                      action: {
-                      type: "uri",
-                      label: "WEBSITE",
-                      uri: "http://mobile.gnavi.co.jp/shop/t006913/?ak=FttuwHWr4RPF4xpOulk3Z1ouZrp1pzxSR2g80sgnVZc%3D"
-                    }
-                  }
-                ]
-              }
-            }
           }
 
         client.reply_message(event['replyToken'], buf_test)
