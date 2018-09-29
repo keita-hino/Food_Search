cf17a5ef872ae931c7f128a2668d95880488cadc
日野恵太 <hinokeita@hinokeitanoMacBook-puro.local>
2018-09-29 15:27:14 +0900

    test

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -34,116 +34,50 @@ class LinebotController < ApplicationController
           #   text: "testです"
           # }
           buf_test = {
-          type: "bubble",
-          hero: {
-            type: "image",
-            url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_2_restaurant.png",
-            size: "full",
-            aspectRatio: "20:13",
-            aspectMode: "cover",
-            action: {
-              type: "uri",
-              uri: "https://linecorp.com"
-            }
-          },
-          body: {
-            type: "box",
-            layout: "vertical",
-            spacing: "md",
-            action: {
-              type: "uri",
-              uri: "https://linecorp.com"
-            },
-            contents: [
-              {
-                type: "text",
-                text: "Brown's Burger",
-                size: "xl",
-                weight: "bold"
-              },
-              {
-                type: "box",
-                layout: "vertical",
-                spacing: "sm",
-                contents: [
-                  {
-                    type: "box",
-                    layout: "baseline",
-                    contents: [
-                      {
-                        type: "icon",
-                        url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/restaurant_regular_32.png"
-                      },
-                      {
-                        type: "text",
-                        text: "$10.5",
-                        weight: "bold",
-                        margin: "sm",
-                        flex: 0
-                      },
-                      {
-                        type: "text",
-                        text: "400kcl",
-                        size: "sm",
-                        align: "end",
-                        color: "#aaaaaa"
-                      }
-                    ]
-                  },
-                  {
-                    type: "box",
-                    layout: "baseline",
-                    contents: [
-                      {
-                        type: "icon",
-                        url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/restaurant_large_32.png"
-                      },
-                      {
-                        type: "text",
-                        text: "$15.5",
-                        weight: "bold",
-                        margin: "sm",
-                        flex: 0
-                      },
-                      {
-                        type: "text",
-                        text: "550kcl",
-                        size: "sm",
-                        align: "end",
-                        color: "#aaaaaa"
-                      }
-                    ]
-                  }
-                ]
-              },
-              {
-                type: "text",
-                text: "Sauce, Onions, Pickles, Lettuce & Cheese",
-                wrap: true,
-                color: "#aaaaaa",
-                size: "xxs"
-              }
-            ]
-          },
-          footer: {
-            type: "box",
-            layout: "vertical",
-            contents: [
-              {
-                type: "spacer",
-                size: "xxl"
-              },
-              {
-                type: "button",
-                style: "primary",
-                color: "#905c44",
-                action: {
-                  type: "uri",
-                  label: "Add to Cart",
-                  uri: "https://linecorp.com"
+          type: "flex",
+          altText: "this is a flex message",
+          contents: {
+            type: "bubble",
+            header: {
+              type: "box",
+              layout: "vertical",
+              contents: [
+                {
+                  type: "text",
+                  text: "Header text"
                 }
-              }
-            ]
+              ]
+            },
+            hero: {
+              type: "image",
+              url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_2_restaurant.png",
+              size: "full",
+              aspectRatio: "4:3"
+            },
+            body: {
+              type: "box",
+              layout: "vertical",
+              contents: [
+                {
+                  type: "店名",
+                  text: "Body text",
+                  size: "xl",
+                  weight: "bold",
+                }
+              ]
+            },
+            footer: {
+              type: "box",
+              layout: "vertical",
+              contents: [
+                {
+                  type: "text",
+                  text: "Footer text",
+                  align: "center",
+                  color: "#888888"
+                }
+              ]
+            }
           }
         }
 
