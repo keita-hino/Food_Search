eeb08de2b0609c55b03b1ab85be45920bf62e868
日野恵太 <hinokeita@hinokeitanoMacBook-puro.local>
2018-09-29 10:20:55 +0900

    test

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -27,59 +27,118 @@ class LinebotController < ApplicationController
         case event.type
         when Line::Bot::Event::MessageType::Text
           buf_test =  {
-          "type": "flex",
-          "altText": "hogehoge",
-          "contents": {
-              "type": "bubble",
-              "styles": {
-                  "header": {
-                      "backgroundColor": "#ff62ae"
-                  },
-                  "body": {
-                      "backgroundColor": "#5bff54"
-                  },
-                  "footer": {
-                      "backgroundColor": "#7b78ff"
-                  }
+  "type": "bubble",
+  "hero": {
+    "type": "image",
+    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_2_restaurant.png",
+    "size": "full",
+    "aspectRatio": "20:13",
+    "aspectMode": "cover",
+    "action": {
+      "type": "uri",
+      "uri": "https://linecorp.com"
+    }
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "spacing": "md",
+    "action": {
+      "type": "uri",
+      "uri": "https://linecorp.com"
+    },
+    "contents": [
+      {
+        "type": "text",
+        "text": "Brown's Burger",
+        "size": "xl",
+        "weight": "bold"
+      },
+      {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "box",
+            "layout": "baseline",
+            "contents": [
+              {
+                "type": "icon",
+                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/restaurant_regular_32.png"
               },
-              "header": {
-                  "type": "box",
-                  "layout": "vertical",
-                  "contents": [
-                      {
-                          "type": "text",
-                          "text": "header"
-                      }
-                  ]
+              {
+                "type": "text",
+                "text": "$10.5",
+                "weight": "bold",
+                "margin": "sm",
+                "flex": 0
               },
-              "hero": {
-                  "type": "image",
-                  "url": "<imageUrl>",
-                  "size": "full",
-                  "aspectRatio": "1:1"
-              },
-              "body": {
-                  "type": "box",
-                  "layout": "vertical",
-                  "contents": [
-                      {
-                          "type": "text",
-                          "text": "body"
-                      }
-                  ]
-              },
-              "footer": {
-                  "type": "box",
-                  "layout": "vertical",
-                  "contents": [
-                      {
-                          "type": "text",
-                          "text": "footer"
-                      }
-                  ]
+              {
+                "type": "text",
+                "text": "400kcl",
+                "size": "sm",
+                "align": "end",
+                "color": "#aaaaaa"
               }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "baseline",
+            "contents": [
+              {
+                "type": "icon",
+                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/restaurant_large_32.png"
+              },
+              {
+                "type": "text",
+                "text": "$15.5",
+                "weight": "bold",
+                "margin": "sm",
+                "flex": 0
+              },
+              {
+                "type": "text",
+                "text": "550kcl",
+                "size": "sm",
+                "align": "end",
+                "color": "#aaaaaa"
+              }
+            ]
           }
+        ]
+      },
+      {
+        "type": "text",
+        "text": "Sauce, Onions, Pickles, Lettuce & Cheese",
+        "wrap": true,
+        "color": "#aaaaaa",
+        "size": "xxs"
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "spacer",
+        "size": "xxl"
+      },
+      {
+        "type": "button",
+        "style": "primary",
+        "color": "#905c44",
+        "action": {
+          "type": "uri",
+          "label": "Add to Cart",
+          "uri": "https://linecorp.com"
+        }
+      }
+    ]
   }
+}
 
           message = {
             type: 'text',
