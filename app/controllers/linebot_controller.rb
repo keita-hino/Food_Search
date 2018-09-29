90f61967c2042c63e048bddaeb2176c14e1297c1
日野恵太 <hinokeita@hinokeitanoMacBook-puro.local>
2018-09-29 15:53:58 +0900

    test

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -53,51 +53,51 @@ class LinebotController < ApplicationController
                     text: "スターバックスコーヒー",
                     weight: "bold",
                     size: "xl"
-                  }
+                  },
+                  {
+                    type: "box",
+                    layout: "baseline",
+                    margin: "md",
+                    # 星の評価の部分
+                    contents: [
+                      {
+                        type: "icon",
+                        size: "sm",
+                        url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                      },
+                      {
+                        type: "icon",
+                        size: "sm",
+                        url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                      },
+                      {
+                        type: "icon",
+                        size: "sm",
+                        url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                      },
+                      {
+                        type: "icon",
+                        size: "sm",
+                        url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                      },
+                      {
+                        type: "icon",
+                        size: "sm",
+                        url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
+                      },
+                      {
+
+                        type: "text",
+                        text: "4.0",
+                        size: "sm",
+                        color: "#999999",
+                        margin: "md",
+                        flex: 0
+                      }
+                    ]
+                  },
                 ]
               },
-            #   {
-            #   type: "box",
-            #   layout: "baseline",
-            #   margin: "md",
-            #   # 星の評価の部分
-            #   contents: [
-            #     {
-            #       type: "icon",
-            #       size: "sm",
-            #       url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-            #     },
-            #     {
-            #       type: "icon",
-            #       size: "sm",
-            #       url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-            #     },
-            #     {
-            #       type: "icon",
-            #       size: "sm",
-            #       url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-            #     },
-            #     {
-            #       type: "icon",
-            #       size: "sm",
-            #       url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-            #     },
-            #     {
-            #       type: "icon",
-            #       size: "sm",
-            #       url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-            #     },
-            #     {
-            #
-            #       type: "text",
-            #       text: "4.0",
-            #       size: "sm",
-            #       color: "#999999",
-            #       margin: "md",
-            #       flex: 0
-            #     }
-            #   ]
-            # },
               footer: {
                 type: "box",
                 layout: "vertical",
