fedad1588d44bf2717e6c8d42bb25963820ed80e
日野恵太 <hinokeita@hinokeitanoMacBook-puro.local>
2018-10-02 10:56:07 +0900

    test

--- app/lib/line.rb
+++ app/lib/line.rb
@@ -543,7 +543,139 @@ class LineJson
           }
         ]
       }
+    },
+    #複数表示
+    {
+    type: "bubble",
+    hero: {
+      type: "image",
+      url: reply[3]["shop_image"],
+      size: "full",
+      aspectRatio: "20:13"
+    },
+    body: {
+      type: "box",
+      layout: "vertical",
+      contents: [
+        {
+          type: "text",
+          text: reply[3]["name"],
+          weight: "bold",
+          size: "xl"
+        },
+        {
+          type: "box",
+          layout: "baseline",
+          margin: "md",
+          # 星の評価の部分
+          contents: [
+            {
+              type: "icon",
+              size: "sm",
+              url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+            },
+            {
+              type: "icon",
+              size: "sm",
+              url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+            },
+            {
+              type: "icon",
+              size: "sm",
+              url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+            },
+            {
+              type: "icon",
+              size: "sm",
+              url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+            },
+            {
+              type: "icon",
+              size: "sm",
+              url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
+            },
+            {
+
+              type: "text",
+              text: "4.0",
+              size: "sm",
+              color: "#999999",
+              margin: "md",
+              flex: 0
+            }
+          ]
+        },
+      {
+        type: "box",
+        layout: "vertical",
+        margin: "lg",
+        spacing: "sm",
+        contents: [
+          {
+            type: "box",
+            layout: "baseline",
+            spacing: "sm",
+            contents: [
+              {
+                type: "text",
+                text: "Place",
+                color: "#aaaaaa",
+                size: "sm",
+                flex: 1
+              },
+              {
+                type: "text",
+                text: reply[3]["address"],
+                wrap: true,
+                color: "#666666",
+                size: "sm",
+                flex: 5
+              }
+            ]
+          },
+          {
+            type: "box",
+            layout: "baseline",
+            spacing: "sm",
+            contents: [
+              {
+                type: "text",
+                text: "Time",
+                color: "#aaaaaa",
+                size: "sm",
+                flex: 1
+              },
+              {
+                type: "text",
+                text: reply[3]["opentime"],
+                wrap: true,
+                color: "#666666",
+                size: "sm",
+                flex: 5
+              }
+            ]
+          }
+        ]
+      }
+      ]
+    },
+    footer: {
+      type: "box",
+      layout: "vertical",
+      contents: [
+        {
+          type: "button",
+          style: "link",
+          height: "sm",
+            action: {
+            type: "uri",
+            label: "WEBSITE",
+            uri: reply[3]["url_mobile"]
+          }
+        }
+      ]
     }
+  }
 
 
 
