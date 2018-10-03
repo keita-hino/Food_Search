e7805cf5151a366844a5e955a0bc1ccd914d6d37
日野恵太 <hinokeita@hinokeitanoMacBook-puro.local>
2018-10-03 11:33:21 +0900

    feat:複数のお店を表示する機能追加

--- app/lib/processing.rb
+++ app/lib/processing.rb
@@ -15,7 +15,7 @@ class Processer
       count = 4
     end
 
-    for i in 0..count - 1
+    for i in 0..count 
       buf[i] = {
         "name" => rest[i]["name"],
         "category" => rest[i]["category"],
