3d8353626f9677a1a81ab7ad67ffeed9acf5d081
日野恵太 <hinokeita@hinokeitanoMacBook-puro.local>
2018-10-04 12:27:09 +0900

    feat:help機能修正

--- app/lib/command.rb
+++ app/lib/command.rb
@@ -15,106 +15,115 @@ class Command
 
   def get_help_test
     a = {
-      type: "bubble",
-      header: {
-        type: "box",
-        layout: "vertical",
+      type: "flex",
+      altText: "this is a flex message",
+      contents: {
+        type: "carousel",
         contents: [
           {
-            type: "text",
-            text: "help",
-            weight: "bold",
-            color: "#aaaaaa",
-            size: "xl"
+            type: "bubble",
+            header: {
+              type: "box",
+              layout: "vertical",
+              contents: [
+                {
+                  type: "text",
+                  text: "help",
+                  weight: "bold",
+                  color: "#aaaaaa",
+                  size: "xl"
+                }
+              ]
+            },
+            body: {
+              type: "box",
+              spacing: "xxl",
+              layout: "vertical",
+              contents: [
+                {
+                  type: "box",
+                  layout: "vertical",
+                  contents: [
+                    {
+                      type: "text",
+                      text: "下記のアイコンを押下",
+                      size: "xs",
+                      weight: "bold",
+                      gravity: "bottom",
+                      color: "#666666"
+                    }
+                  ]
+                },
+                {
+                  type: "box",
+                  layout: "vertical",
+                  contents: [
+                    {
+                      type: "image",
+                      url: "https://food-line.herokuapp.com/man1.png",
+                      aspectMode: "cover",
+                      aspectRatio: "20:13",
+                      gravity: "top",
+                      size: "full"
+                    }
+                  ]
+                },
+                {
+                  type: "box",
+                  layout: "vertical",
+                  contents: [
+                    {
+                      type: "text",
+                      text: "「この位置を送信」を押下",
+                      size: "xs",
+                      weight: "bold",
+                      color: "#666666"
+                    }
+                  ]
+                },
+                {
+                  type: "box",
+                  layout: "vertical",
+                  contents: [
+                    {
+                      type: "image",
+                      url: "https://food-line.herokuapp.com/man2.png",
+                      aspectMode: "cover",
+                      aspectRatio: "3:4",
+                      size: "full"
+                    }
+                  ]
+                },
+                {
+                  type: "box",
+                  layout: "vertical",
+                  contents: [
+                    {
+                      type: "text",
+                      text: "送信ボタンを押下",
+                      size: "xs",
+                      weight: "bold",
+                      color: "#666666"
+                    }
+                  ]
+                },
+                {
+                  type: "box",
+                  layout: "vertical",
+                  contents: [
+                    {
+                      type: "image",
+                      url: "https://food-line.herokuapp.com/man3.png",
+                      aspectMode: "cover",
+                      aspectRatio: "3:4",
+                      size: "full"
+                    }
+                  ]
+                }
+              ]
+            }
           }
-        ]
-      },
-      body: {
-        type: "box",
-        spacing: "xxl",
-        layout: "vertical",
-        contents: [
-          {
-            type: "box",
-            layout: "vertical",
-            contents: [
-              {
-                type: "text",
-                text: "下記のアイコンを押下",
-                size: "xs",
-                weight: "bold",
-                gravity: "bottom",
-                color: "#666666"
-              }
-            ]
-          },
-          {
-            type: "box",
-            layout: "vertical",
-            contents: [
-              {
-                type: "image",
-                url: "https://food-line.herokuapp.com/man1.png",
-                aspectMode: "cover",
-                aspectRatio: "20:13",
-                gravity: "top",
-                size: "full"
-              }
-            ]
-          },
-          {
-            type: "box",
-            layout: "vertical",
-            contents: [
-              {
-                type: "text",
-                text: "「この位置を送信」を押下",
-                size: "xs",
-                weight: "bold",
-                color: "#666666"
-              }
-            ]
-          },
-          {
-            type: "box",
-            layout: "vertical",
-            contents: [
-              {
-                type: "image",
-                url: "https://food-line.herokuapp.com/man2.png",
-                aspectMode: "cover",
-                aspectRatio: "3:4",
-                size: "full"
-              }
-            ]
-          },
-          {
-            type: "box",
-            layout: "vertical",
-            contents: [
-              {
-                type: "text",
-                text: "送信ボタンを押下",
-                size: "xs",
-                weight: "bold",
-                color: "#666666"
-              }
-            ]
-          },
-          {
-            type: "box",
-            layout: "vertical",
-            contents: [
-              {
-                type: "image",
-                url: "https://food-line.herokuapp.com/man3.png",
-                aspectMode: "cover",
-                aspectRatio: "3:4",
-                size: "full"
-              }
-            ]
-          }
-        ]
+        }]
       }
     }
   end
