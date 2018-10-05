951083d1b3a7a33d709e4f4abea1932fdbd2e776
日野恵太 <hinokeita@hinokeitanoMacBook-puro.local>
2018-10-05 13:29:18 +0900

    tets

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -64,7 +64,7 @@ class LinebotController < ApplicationController
       when Line::Bot::Event::Postback
           message = {
             type: 'text',
-            text: event.postback['date']
+            text: event.postback['data']
           }
             client.reply_message(event['replyToken'], message)
       end
