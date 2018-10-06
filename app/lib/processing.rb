a167ddd7a00ceb92035eb4998e15f0d327735cfb
日野恵太 <hinokeita@hinokeitanoMacBook-puro.local>
2018-10-06 13:44:16 +0900

    test

--- app/lib/processing.rb
+++ app/lib/processing.rb
@@ -15,6 +15,9 @@ class Processer
     end
 
     for i in 0..count
+      if rest[i]["latitude"] == ""
+        next
+      end
       buf[i] = {
         "name" => rest[i]["name"],
         "category" => rest[i]["category"],
