809ab8412c7d53b28d3909a0c95dfce9f56a8f69
日野恵太 <hinokeita@hinokeitanoMacBook-puro.local>
2018-10-06 11:06:06 +0900

    test

--- app/lib/command.rb
+++ app/lib/command.rb
@@ -13,66 +13,6 @@ class Command
     return buf
   end
 
-  def get_help_test
-    a = {type: bubble
-header:
-  type: box
-  layout: vertical
-  contents:
-    - type: text
-      text: help
-      weight: bold
-      color: '#aaaaaa'
-      size: xl
-body:
-  type: box
-  spacing: xxl
-  layout: vertical
-  contents:
-    - type: box
-      layout: vertical
-      contents:
-        - type: text
-          text: 下記のアイコンを押下
-          size: xs
-          weight: bold
-          gravity: bottom
-          color: '#00bfff'
-        - type: image
-          url: 'https://food-line.herokuapp.com/man1.png'
-          aspectMode: cover
-          aspectRatio: '20:13'
-          gravity: top
-          size: full
-    - type: box
-      layout: vertical
-      contents:
-        - type: text
-          text: 「この位置を送信」を押下
-          size: xs
-          weight: bold
-          color: '#00bfff'
-        - type: image
-          url: 'https://food-line.herokuapp.com/man2.png'
-          aspectMode: cover
-          aspectRatio: '3:4'
-          size: full
-    - type: box
-      layout: vertical
-      contents:
-        - type: text
-          text: 送信ボタンを押下
-          size: xs
-          weight: bold
-          color: '#00bfff'
-        - type: image
-          url: 'https://food-line.herokuapp.com/man3.png'
-          aspectMode: cover
-          aspectRatio: '3:4'
-          size: full
-}
-  end
-
   # def get_help_test
   #   buf = Hash.new
   #   buf["type"] = "flex"
