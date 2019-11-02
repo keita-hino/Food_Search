c55f0978b165c8ec957c3bc29d430503bc813bd5
日野恵太 <keitahino0415@gmail.com>
2019-11-02 09:23:34 +0900

    シェア機能の途中まで実装

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,6 +3,12 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new
     @restaurants = Restaurant.user_uid_is(current_user.uid)
                              .order('updated_at desc')
+
+    @users = Request.new(
+            client: client,
+            users: Lineuser.all
+          ).user_list
+
   end
 
   def create
@@ -24,6 +30,13 @@ class RestaurantsController < ApplicationController
         :address,
         :open_info
       )
+    end
+
+    def client
+      @client ||= Line::Bot::Client.new { |config|
+        config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+        config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      }
     end
 
 end
