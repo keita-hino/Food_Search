5cc712a115de1ad2dc792e01b7b244fd2e40762b
日野恵太 <keitahino0415@gmail.com>
2019-11-03 14:54:37 +0900

    awsのホストをホワイトリストに追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
   config.hosts << '.ngrok.io'
+  config.hosts << "ec2-3-112-219-26.ap-northeast-1.compute.amazonaws.com"
   # Do not eager load code on boot.
   config.eager_load = false
 
