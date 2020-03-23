0e5992b61941e479bb4507a6854aa7686814145c
日野恵太 <keitahino0415@gmail.com>
2020-03-23 12:08:44 +0900

    見出しのデザイン微修正

--- app/javascript/components/restaurants/Index.vue
+++ app/javascript/components/restaurants/Index.vue
@@ -6,7 +6,7 @@
           class="lighten-4"
           justify="center" align-content="start"
         >
-          <div class='display-2 font-weight-regular	'>お店一覧</div>
+          <div class='headline'>お店一覧</div>
         </v-row>
 
         <!-- モーダル -->
