a8717d627de87c3d0f26c4e32982206ff0325734
日野恵太 <keitahino0415@gmail.com>
2020-03-29 21:28:57 +0900

    VueScrollToの設定変更

--- app/javascript/packs/scrollto.js
+++ app/javascript/packs/scrollto.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import VueScrollTo from 'vue-scrollto'
 
-Vue.use(VueScrollTo, {
+Vue.use(VueScrollTo);
+
+const scrollto = {
   /* オプション色々あります。公式サイトから確認できます。 */
   duration: 1000,
   easing: "ease"
-})
+};
+
+export default new VueScrollTo({ scrollto });
