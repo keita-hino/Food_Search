e713f2e199ea8972a2582e443450ae304a740500
日野恵太 <keitahino0415@gmail.com>
2020-03-31 17:49:46 +0900

    webpackerの設定変更

--- config/webpack/loaders/vue.js
+++ config/webpack/loaders/vue.js
@@ -1,9 +1,6 @@
 module.exports = {
   test: /\.vue(\.erb)?$/,
   use: [{
-    loader: 'vue-loader',
-    options: PnpWebpackPlugin.tsLoaderOptions({
-      appendTsSuffixTo: [/\.vue$/]
-    })
+    loader: 'vue-loader'
   }]
 }
